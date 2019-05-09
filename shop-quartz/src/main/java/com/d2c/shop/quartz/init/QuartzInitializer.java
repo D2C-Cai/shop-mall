b13190f75bafac19028f76eebd5c8e07e2386274
BaiCai <709931138@qq.com>
2019-05-09 17:43:52 +0800

    文档错误纠正

--- shop-quartz/src/main/java/com/d2c/shop/quartz/init/QuartzInitializer.java
+++ shop-quartz/src/main/java/com/d2c/shop/quartz/init/QuartzInitializer.java
@@ -2,35 +2,50 @@ package com.d2c.shop.quartz.init;
 
 import com.d2c.shop.quartz.handler.QuartzHandler;
 import com.d2c.shop.quartz.jobs.base.BaseJob;
-import com.d2c.shop.service.common.utils.SpringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import java.util.Map;
 
 /**
  * @author Cai
  */
+@Slf4j
 @Component
-public class QuartzInitializer {
+public class QuartzInitializer implements ServletContextListener {
 
     @Autowired
     private Scheduler scheduler;
     @Autowired
     private QuartzHandler quartzHandler;
 
-    @PostConstruct
-    public void init() throws SchedulerException {
-        Map<String, BaseJob> map = SpringUtil.getApplicationContext().getBeansOfType(BaseJob.class);
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+        Map<String, BaseJob> map = applicationContext.getBeansOfType(BaseJob.class);
         for (BaseJob job : map.values()) {
-            if (scheduler.getJobDetail(new JobKey(job.getClass().getName())) == null) {
-                quartzHandler.insert(job.getClass().getName(), job.getCronExpression());
+            try {
+                if (scheduler.getJobDetail(new JobKey(job.getClass().getName())) == null) {
+                    quartzHandler.insert(job.getClass().getName(), job.getCronExpression());
+                }
+            } catch (SchedulerException e) {
+                log.error(e.getMessage(), e);
             }
         }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
     }
 
 }
